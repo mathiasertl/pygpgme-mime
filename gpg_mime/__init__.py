bd51ebf5e16a75004fddbd74603cdb3ec26917bc
Mathias Ertl <mati@er.tl>
2016-07-10 01:55:26 +0200

    try to implement singning

--- gpg_mime/__init__.py
+++ gpg_mime/__init__.py
@@ -15,13 +15,15 @@
 
 from __future__ import unicode_literals, absolute_import
 
+from email.mime.application import MIMEApplication
+
 import gpgme
 import six
 
-from six.moves.email_mime_application import MIMEApplication
 from six.moves.email_mime_multipart import MIMEMultipart
 from six.moves.email_mime_text import MIMEText
-
+from six.moves.email_mime_base import MIMEBase
+from email.encoders import encode_noop
 
 
 def rfc3156(message, recipients=None, signers=None, context=None, always_trust=False):
@@ -36,28 +38,35 @@ def rfc3156(message, recipients=None, signers=None, context=None, always_trust=F
         If ``True``, always trust recipient keys.
     """
 
-    if not signers and not recipients:
+    if (not signers and not recipients) or context and context.signers:
         raise ValueError("No signers or recipients given.")
 
     if isinstance(message, six.string_types):
-        message = MIMEText(message, _charset='utf-8')
+        message = MIMEText(message)
+        del message['MIME-Version']
+
     if recipients is None:
         recipients = []
+    elif isinstance(recipients, six.string_types):
+        recipients = [recipients]
+
     if signers is None:
         signers = []
+    elif isinstance(signers, six.string_types):
+        signers = [signers]
 
     if context is None:
         context = gpgme.Context()
     context.armor = True
 
     # signers/recpiients may either be a string or a key from the context
-    signers = [context.get_key(k) if isinstance(k, six.string_types) else k for k in signers]
+    signers = [(context.get_key(k) if isinstance(k, six.string_types) else k) for k in signers]
     recipients = [context.get_key(k) if isinstance(k, six.string_types) else k for k in recipients]
 
     if signers:
         context.signers = signers
 
-    input_bytes = six.BytesIO(message.as_string())
+    input_bytes = six.BytesIO(message.as_bytes())
     output_bytes = six.BytesIO()
 
     if recipients:  # we have recipients, so we encrypt
@@ -67,6 +76,7 @@ def rfc3156(message, recipients=None, signers=None, context=None, always_trust=F
         if always_trust is True:
             flags |= gpgme.ENCRYPT_ALWAYS_TRUST
 
+        # sign message
         if context.signers:
             context.encrypt_sign(recipients, flags, input_bytes, output_bytes)
         else:
@@ -74,16 +84,38 @@ def rfc3156(message, recipients=None, signers=None, context=None, always_trust=F
         output_bytes.seek(0)
 
         # the control message
-        control_msg = MIMEApplication(_subtype='pgp-encrypted')
+        control_msg = MIMEApplication(_data='Version: 1\n', _subtype='pgp-encrypted',
+                                      _encoder=encode_noop)
         control_msg.add_header('Content-Description', 'PGP/MIME version identification')
-        control_msg.set_payload('Version: 1\n')
+        del control_msg['MIME-Version']
 
-        encrypted = MIMEApplication(_subtype='octed-stream', anme='encrypted.asc')
+        encrypted = MIMEApplication(_data=output_bytes.getvalue(),
+                                    _subtype='octed-stream', name='encrypted.asc',
+                                    _encoder=encode_noop)
         encrypted.add_header('Content-Description', 'OpenPGP encrypted message')
         encrypted.add_header('Content-Disposition', 'inline; filename="encrypted.asc"')
-        encrypted.set_payload(output_bytes.getvalue())
+        del encrypted['MIME-Version']
 
         msg = MIMEMultipart(_subtype='pgp-encrypted', _subparts=[control_msg, encrypted])
+        msg.set_param('protocol', 'application/pgp-encrypted')
         return msg
     else:  # just signing
-        pass
+        context.sign(input_bytes, output_bytes, gpgme.SIG_MODE_DETACH)
+        output_bytes.seek(0)
+        signature = output_bytes.getvalue()
+
+        with open('message', 'wb') as fp:
+            fp.write(message.as_bytes())
+        with open('message.sig', 'wb') as fp:
+            fp.write(signature)
+
+        sig = MIMEBase(_maintype='application', _subtype='pgp-signature', name='signature.asc')
+        sig.set_payload(signature)
+        sig.add_header('Content-Description', 'OpenPGP digital signature')
+        sig.add_header('Content-Disposition', 'attachment; filename="signature.asc"')
+        del sig['MIME-Version']
+        del sig['Content-Transfer-Encoding']
+
+        msg = MIMEMultipart(_subtype='signed', _subparts=[message, sig])
+        msg.set_param('protocol', 'application/pgp-signature')
+        return msg
